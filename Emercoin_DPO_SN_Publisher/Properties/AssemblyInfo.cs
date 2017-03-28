6abf9ac491c427a2f2ee66cf265f0e653aeebe6b
Max <fretnoiz@yandex.ru>
2017-03-28 15:29:02 +0300

    - Renamed latest utility version to 2.1. - Published previous version in the Distrib folder.

--- Emercoin_DPO_SN_Publisher/Properties/AssemblyInfo.cs
+++ Emercoin_DPO_SN_Publisher/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.18.0")]
+[assembly: AssemblyFileVersion("2.1.18.0")]
